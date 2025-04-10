41814deed210f133bcf817650e764805e19afea4
dkulemin <blablamaildk@gmail.com>
2025-04-10 22:36:14 +0500

    gm addon example

---
+++ Goldenmod/golden_speech_mod/README.md
@@ -0,0 +1,1 @@
+Эту папку нужно скопировать в C:\Program Files (x86)\Steam\steamapps\common\Gothic\launcher и оттуда уже запустить батник
---
+++ Goldenmod/golden_speech_mod/publish.bat
@@ -0,0 +1,2 @@
+..\GothicSteamWorkshopUploader.exe publish --game=g1 --title="Озвучка для Золотого мода" --tags="Addon,Dubbing" --content="%cd%\\Content" --preview="%cd%\\preview.jpg"
+pause
